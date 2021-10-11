d67d1037839822ec9e7e364ee4a52f2c3dfb2098
Cartoonish Villain <jedijoe93@gmail.com>
2021-10-11 10:49:45 -0400

    Added config options

--- src/main/java/com/cartoonishvillain/trickxortreat/TrickXORTreat.java
+++ src/main/java/com/cartoonishvillain/trickxortreat/TrickXORTreat.java
@@ -1,12 +1,16 @@
 package com.cartoonishvillain.trickxortreat;
 
+import com.cartoonishvillain.trickxortreat.config.CommonConfig;
+import com.cartoonishvillain.trickxortreat.config.ConfigHelper;
 import com.cartoonishvillain.trickxortreat.items.Candy;
+import net.minecraft.item.Item;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -23,7 +27,8 @@ public class TrickXORTreat
 
     public static ArrayList<SoundEvent> tricksounds = new ArrayList<SoundEvent>(Arrays.asList(SoundEvents.ZOMBIE_VILLAGER_CURE, SoundEvents.ZOMBIE_VILLAGER_AMBIENT, SoundEvents.BELL_RESONATE, SoundEvents.PARROT_IMITATE_ENDER_DRAGON, SoundEvents.PHANTOM_AMBIENT, SoundEvents.VEX_CHARGE, SoundEvents.TOTEM_USE));
     public static ArrayList<Effect> trickeffects = new ArrayList<Effect>(Arrays.asList(Effects.LEVITATION, Effects.WEAKNESS, Effects.MOVEMENT_SLOWDOWN, Effects.UNLUCK, Effects.BLINDNESS, Effects.CONFUSION));
-    public static ArrayList<Candy> candies;
+    public static ArrayList<Item> candies;
+    public static CommonConfig config;
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "trickxortreat";
@@ -31,6 +36,7 @@ public class TrickXORTreat
     public TrickXORTreat() {
         // Register the setup method for modloading
         Register.init();
+        config = ConfigHelper.register(ModConfig.Type.COMMON, CommonConfig::new);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -38,7 +44,7 @@ public class TrickXORTreat
 
     private void setup(final FMLCommonSetupEvent event)
     {
-        candies = new ArrayList<Candy>(Arrays.asList(Register.CREEPERCOCHOCOLATE.get(), Register.FLAMINGDOTS.get(), Register.SUGARBONE.get(), Register.CLOUDCANDY.get(), Register.DOLPHINCRUNCH.get(), Register.HEROICBAR.get(), Register.PIGMENTCOIN.get(), Register.RABBITCANDY.get(), Register.CREEPERCOCHOCOLATE.get(), Register.FLAMINGDOTS.get(), Register.SUGARBONE.get(), Register.CLOUDCANDY.get(), Register.DOLPHINCRUNCH.get(), Register.HEROICBAR.get(), Register.PIGMENTCOIN.get(), Register.RABBITCANDY.get(), Register.PRISMATICFLESH.get()));
+        candies = new ArrayList<Item>(Arrays.asList(Register.CREEPERCOCHOCOLATE.get(), Register.FLAMINGDOTS.get(), Register.SUGARBONE.get(), Register.CLOUDCANDY.get(), Register.DOLPHINCRUNCH.get(), Register.HEROICBAR.get(), Register.PIGMENTCOIN.get(), Register.RABBITCANDY.get(), Register.CREEPERCOCHOCOLATE.get(), Register.FLAMINGDOTS.get(), Register.SUGARBONE.get(), Register.CLOUDCANDY.get(), Register.DOLPHINCRUNCH.get(), Register.HEROICBAR.get(), Register.PIGMENTCOIN.get(), Register.RABBITCANDY.get(), Register.PRISMATICFLESH.get()));
     }
 
 
---
+++ src/main/java/com/cartoonishvillain/trickxortreat/config/CommonConfig.java
@@ -0,0 +1,19 @@
+package com.cartoonishvillain.trickxortreat.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class CommonConfig {
+    public static final String CATEGORY = "Magical Properties";
+
+    public ConfigHelper.ConfigValueListener<Integer> baseTrickChance;
+    public ConfigHelper.ConfigValueListener<Integer> baseTreatChance;
+
+    public CommonConfig(ForgeConfigSpec.Builder builder, ConfigHelper.Subscriber subscriber){
+        builder.comment("Modify the rules for the enchantments").push(CATEGORY);
+
+        baseTrickChance = subscriber.subscribe(builder.comment("Changes the base chance of the effects of trick happening. Multiplied by 2 and 3 for higher tiers of the enchantment.").defineInRange("baseTrickChance", 5, 1, 100));
+        baseTreatChance = subscriber.subscribe(builder.comment("Changes the base chance of the effects of treat happening. Multiplied by 2 and 3 for higher tiers of the enchantment.").defineInRange("baseTreatChance", 5, 1, 100));
+    }
+}
